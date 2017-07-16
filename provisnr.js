14dca95f1da29caa52da70d611cda8be8b85395a
neven <neven@sofiahardcode.com>
2017-07-16 21:54:55 +0300

    Update non-minified core

--- provisnr.js
+++ provisnr.js
@@ -18,7 +18,8 @@
                 iterations: {},
                 fasterFunction: {
                     name: "",
-                    fasterInPercentage: 0
+                    fasterInPercentage: 0,
+                    fasterInIterations: 0
                 }
             };
 
@@ -35,6 +36,7 @@
             if ( ! equal ) {
                 results.fasterFunction.name = faster.name;
                 results.fasterFunction.fasterInPercentage = parseInt((faster.iterations - slower.iterations) / slower.iterations * 100, 10);
+                results.fasterFunction.fasterInIterations = faster.iterations - slower.iterations;
             } else {
                 results.equallyFast = true;
             }
@@ -43,6 +45,47 @@
             results.iterations[b.name] = b.iterations;
 
             return results;
+        },
+        issueError: function(error) {
+            return new Error(error);
+        },
+        testFunction: function(options) {
+            var that = base;
+
+            if ( typeof options !== "object" ) {
+                return that.issueError("Provisnr requires options (object). Pass an options object.");
+            }
+
+            if ( typeof options.type !== "string" ) {
+                return that.issueError("Provisnr requires a type (string). Pass a type ('procedural'/'async') as part of the options object.");
+            }
+
+            if ( typeof options.timeout !== "number" ) {
+                return that.issueError("Provisnr requires a timeout (integer). Pass a timeout property as part of the options object.");
+            }
+
+            if ( typeof options.callback !== "function" ) {
+                return that.issueError("Provisnr requires a callback (function). Pass a callback as part of the options object.");
+            }
+
+            if ( options.type !== "procedural" && typeof options.complete !== "function" ) {
+                return that.issueError("Provisnr requires a complete (function). Pass a complete as part of the options object.");
+            }
+
+            if ( options.type === "procedural" ) {
+                base.testProceFunction({
+                    timeout: parseInt(options.timeout, 10),
+                    callback: options.callback,
+                    generator: options.generator
+                });
+            } else {
+                base.testAsyncFunction({
+                    timeout: parseInt(options.timeout, 10),
+                    callback: options.callback,
+                    generator: options.generator,
+                    complete: options.complete
+                });
+            }
         },
         testProceFunction: function(args) {
             var iterations;
@@ -83,12 +126,38 @@
 
             iterateAsync();
         },
-        comparePerformance: function(args) {
-            var type = args.type;
-            var generator = args.generator || function() {};
-            var timeout = args.timeout;
-            var complete = args.complete;
-            var callbacks = args.callbacks;
+        comparePerformance: function(options) {
+            var that = base;
+
+            if ( typeof options !== "object" ) {
+                return that.issueError("Provisnr requires options (object). Pass an options object.");
+            }
+
+            if ( typeof options.type !== "string" ) {
+                return that.issueError("Provisnr requires a type (string). Pass a type ('procedural'/'async') as part of the options object.");
+            }
+
+            if ( typeof options.timeout !== "number" ) {
+                return that.issueError("Provisnr requires a timeout (integer). Pass a timeout property as part of the options object.");
+            }
+
+            if ( typeof options.callbacks !== "object" ) {
+                return that.issueError("Provisnr requires a callback (function). Pass a callback as part of the options object.");
+            }
+
+            if ( Object.keys(options.callbacks).length !== 2 ) {
+                return that.issueError("Provisnr requires callbacks (object), containing 2 methods for test comparison. Pass two methods to callbacks as part of the options object.");
+            }
+
+            if ( options.type !== "procedural" && typeof options.complete !== "function" ) {
+                return that.issueError("Provisnr requires a complete (function). Pass a complete as part of the options object.");
+            }
+
+            var type = options.type;
+            var generator = options.generator || function() {};
+            var timeout = options.timeout;
+            var complete = options.complete;
+            var callbacks = options.callbacks;
             var keys = Object.keys(callbacks);
             var funcAName = keys[0];
             var funcBName = keys[1];
@@ -139,8 +208,7 @@
 
     var provisnr = function() {
         return {
-            testProceFunction: base.testProceFunction,
-            testAsyncFunction: base.testAsyncFunction,
+            testFunction: base.testFunction,
             comparePerformance: base.comparePerformance
         };
     };
